f93c5cf36a817abc9661e411bbcabbbe49c028ce
sangmin99554 <sangmin99554@naver.com>
2019-11-16 22:59:43 +0900

    commit

--- main_multiprocess.py
+++ main_multiprocess.py
@@ -11,28 +11,28 @@ import frontal_facecrop as frt
 
 STD_SIZE = 128
 
-folder_path = 'D:\\Selected_Img_more'
-save_path = 'D:\\Data\\Error'
+folder_path = 'D:\\Sangmin\\FaceCrop\\image'
+save_path = 'D:\\Sangmin\\FaceCrop\\out'
 
-# glob_path = folder_path + '/*jpg'
-# filenames = glob(glob_path)
+glob_path = folder_path + '/*jpg'
+filenames = glob(glob_path)
 
-filenames = []
+# filenames = []
 
 string = []
 def aaa(result):
     string.append('aaa')
 
 if __name__ == '__main__':
-    with open('./notfound.txt', 'r') as f:
-        filenames = f.readlines()
+    # with open('./notfound.txt', 'r') as f:
+    #     filenames = f.readlines()
 
     pool = mp.Pool(processes=mp.cpu_count())
 
     results = []
     for filename in filenames:  
         # load input image
-        filename = filename.strip()     
+        # filename = filename.strip()     
         image = cv2.imread(os.path.join(folder_path,filename))
         # print(filename)
         # cv2.imshow("test", image)
@@ -42,12 +42,12 @@ if __name__ == '__main__':
         # results.append(result)
 
         ################ DNN ################
-        result = pool.apply_async(dnn.crop_process, (image, filename, folder_path, save_path), callback= aaa)
-        results.append(result)
+        # result = pool.apply_async(dnn.crop_process, (image, filename, folder_path, save_path), callback= aaa)
+        # results.append(result)
 
         ################ Frontal Face ################
-        # result = pool.apply_async(frt.crop_process, (image, filename, folder_path, save_path), callback= aaa)
-        # results.append(result)
+        result = pool.apply_async(frt.crop_process, (image, filename, folder_path, save_path), callback= aaa)
+        results.append(result)
     # pool.close()
     # pool.join()
 
